e7246aca2d9ea7eb3a965eac58e4d6a899ceae6c
Shaun <shaun.danny@jnis.ac.in>
2025-09-28 13:19:53 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,35 @@
+import java.io.*;
+
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+    public static void main(String[] args) {
+        try {
+            Polynomial p1 = new Polynomial(new double[]{5, -3, 0, 7});
+            Polynomial p2 = new Polynomial(new double[]{2, 0, 4});
+
+            System.out.println("p1 = " + p1);
+            System.out.println("p2 = " + p2);
+
+            Polynomial sum = p1.add(p2);
+            System.out.println("p1 + p2 = " + sum);
+
+            Polynomial product = p1.multiply(p2);
+            System.out.println("p1 * p2 = " + product);
+
+            double val = p1.evaluate(2.0);
+            System.out.println("p1(2) = " + val);
+
+            System.out.println("Does p1 have root at x=1? " + p1.hasRoot(1.0));
+
+            String filename = "poly_out.txt";
+            p1.saveToFile(filename);
+            System.out.println("p1 saved to " + filename);
+
+            File file = new File(filename);
+            Polynomial fromFile = new Polynomial(file);
+            System.out.println("Polynomial read from file: " + fromFile);
+
+        } catch (IOException e) {
+            System.out.println("File error: " + e.getMessage());
+        }
     }
 }
